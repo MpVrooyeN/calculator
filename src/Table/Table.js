838a7c0045275c1a3086d74dcaaf230a62e02fd8
Matthew Van Rooyen <46861609+MpVrooyeN@users.noreply.github.com>
2022-10-06 14:39:29 +0200

    Optimized code and fixed answerText render

--- src/Table/Table.js
+++ src/Table/Table.js
@@ -1,22 +1,23 @@
-import { useMemo, useState } from "react";
+import { React, useEffect, useMemo, useState } from "react";
 import { useTable } from "react-table";
 import './TableStyle.css'
-const tableData = [
+const TABLEDATA = [
 	{ first: 7, second: 8, third: 9, fourth: '/' },
 	{ first: 4, second: 5, third: 6, fourth: '*' },
 	{ first: 1, second: 2, third: 3, fourth: '-' },
 	{ first: 0, second: 'C', third: '=', fourth: '+' }]
 
-const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
-const operators = ['/', '*', '-', '+']
-let answerValue = 0
-let secondValue = 0
-let setOperator = null
-let calculated = false
+const NUMBERS = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
+const OPERATORS = ['/', '*', '-', '+']
 
 export const Table = () => {
+
+	const [answerValue, setAnswerValue] = useState(0)
+	const [secondValue, setSecondValue] = useState(0)
+	const [operator, setOperator] = useState(null)
+	const [calculated, setCalculated] = useState(false)
 	const [answerText, setAnswerText] = useState(0)
-	const data = tableData;
+
 	const columns = useMemo(() => [{
 		Header: 'Header',
 		hideHeader: false,
@@ -26,28 +27,22 @@ export const Table = () => {
 			{ Header: '', accessor: 'third' },
 			{ Header: '', accessor: 'fourth' }]
 	}], []);
-	const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } = useTable({ columns, data });
-	function clear() {
-		answerValue = 0
-		secondValue = 0
-		setOperator = null
-		calculated = false
-		console.log("cleared")
-	}
 
-	function calculateOperation() {
-		switch (setOperator) {
+	const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } = useTable({ columns, data: TABLEDATA });
+
+	function calculateResult() {
+		switch (operator) {
 			case '/':
-				answerValue = answerValue / secondValue;
+				setAnswerValue(answerValue / secondValue);
 				break;
 			case '*':
-				answerValue = answerValue * secondValue;
+				setAnswerValue(answerValue * secondValue);
 				break;
 			case '-':
-				answerValue = answerValue - secondValue;
+				setAnswerValue(answerValue - secondValue);
 				break;
 			case '+':
-				answerValue = answerValue + secondValue;
+				setAnswerValue(answerValue + secondValue);
 				break;
 
 			default:
@@ -56,38 +51,54 @@ export const Table = () => {
 	}
 
 	function calculate(cellValue) {
-		if (numbers.includes(cellValue)) {
+
+		if (NUMBERS.includes(cellValue)) {
 			if (calculated) {
-				answerValue = 0
-				secondValue = 0
-				calculated = false
+				setAnswerValue(0)
+				setSecondValue(0)
+				setCalculated(false)
 			}
-			if (setOperator) {
-				secondValue = secondValue * 10 + cellValue
+			if (operator) {
+				let result;
+				result = secondValue * 10 + cellValue
+				setSecondValue(result)
 				console.log("second: " + secondValue)
 			} else {
-				answerValue = answerValue * 10 + cellValue
+				let result;
+				result = answerValue * 10 + cellValue
+				setAnswerValue(result)
 				console.log("answer: " + answerValue)
 			}
 
-		} else if (operators.includes(cellValue)) {
+		} else if (OPERATORS.includes(cellValue)) {
 			if (!calculated) {
-				calculateOperation()
+				calculateResult()
 			}
-			calculated = false
-			setOperator = cellValue
-			secondValue = 0
+
+			setCalculated(false)
+			setOperator(cellValue)
+			setSecondValue(0)
 			console.log('pressed operator: ' + cellValue)
+
 		} else if (cellValue === '=') {
-			calculateOperation()
-			calculated = true
+			calculateResult()
+			setCalculated(true)
 			console.log("pressed equals")
+
 		} else if (cellValue === 'C') {
-			clear()
+			setAnswerValue(0)
+			setSecondValue(0)
+			setOperator(null)
+			setCalculated(false)
+			console.log("cleared")
 		}
+		
 		console.log(answerValue)
-		setAnswerText(answerValue)
 	}
+
+	useEffect(() => {
+		setAnswerText(answerValue.toString());
+	}, [answerValue]);
 
 	return (<div className="container">
 		<p id="answerText" className="value">{answerText}</p>
